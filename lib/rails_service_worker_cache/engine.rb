adaa90a759aa4c01ed42d68047ca3a6cfcfc88b9
Tony Edwards <tony@plymouthsoftware.com>
2016-04-18 17:01:19 +0100

    Update files

--- lib/rails_service_worker_cache/engine.rb
+++ lib/rails_service_worker_cache/engine.rb
@@ -2,11 +2,8 @@ require 'rails_service_worker_cache'
 require 'rails'
 
 module RailsServiceWorkerCache
-  class Engine < Rails::Engine
-    # railtie_name :rails_service_worker_cache
-
-    initializer "static assets" do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+  module Rails
+    class Engine < Rails::Engine
     end
   end
 end
