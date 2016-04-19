c4915a8a9f1e532aacfe907bf10d3e1b2cb0d72c
Tony Edwards <tony@plymouthsoftware.com>
2016-04-19 09:45:50 +0100

    Adds railtie to a module

--- lib/rails_service_worker_cache/rails/railtie.rb
+++ lib/rails_service_worker_cache/rails/railtie.rb
@@ -1,9 +1,11 @@
 module RailsServiceWorkerCache
-  class Railtie < Rails::Railtie
-    railtie_name :rails_service_worker_cache
+  module Rails
+    class Railtie < Rails::Railtie
+      railtie_name :rails_service_worker_cache
 
-    rake_tasks do
-      load "tasks/process_service_worker.rake"
+      rake_tasks do
+        load "tasks/process_service_worker.rake"
+      end
     end
   end
 end
