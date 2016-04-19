954028f1ad5e23f915607ae616909a76594e6f6c
Tony Edwards <tony@plymouthsoftware.com>
2016-04-19 11:22:12 +0100

    Remove console.logs

--- app/assets/javascript/rails-service-worker-cache.js
+++ app/assets/javascript/rails-service-worker-cache.js
@@ -5,16 +5,16 @@ $(document).ready(function(){
 function installCacheServiceWorker(){
   'use strict';
 
-  console.log('Service worker started');
+  // console.log('Service worker started');
   // Check that service workers are available
   if ('serviceWorker' in navigator) {
-  	console.log('navigator available');
+  	// console.log('navigator available');
     navigator.serviceWorker.register('/serviceWorker.js').then(function(reg) {
 
-      console.log('ServiceWorker registration successful with scope: ', reg.scope);
+      // console.log('ServiceWorker registration successful with scope: ', reg.scope);
 
     }).catch(function(err) {
-      console.log('ServiceWorker registration failed: ', err);
+      // console.log('ServiceWorker registration failed: ', err);
     });
   }
 }
