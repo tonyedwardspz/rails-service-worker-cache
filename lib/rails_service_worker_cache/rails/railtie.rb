b2be831b2486197e1c8a8c44431c532e956b34b1
Tony Edwards <tony@plymouthsoftware.com>
2016-04-19 10:00:10 +0100

    Adds more required

--- lib/rails_service_worker_cache/rails/railtie.rb
+++ lib/rails_service_worker_cache/rails/railtie.rb
@@ -1,11 +1,11 @@
-module RailsServiceWorkerCache
-  module Rails
-    class Railtie < Rails::Railtie
-      railtie_name :rails_service_worker_cache
+require 'rails'
 
-      rake_tasks do
-        load "tasks/process_service_worker.rake"
-      end
+module RailsServiceWorkerCache
+  class Railtie < Rails::Railtie
+    railtie_name :rails_service_worker_cache
+
+    rake_tasks do
+      load "tasks/process_service_worker.rake"
     end
   end
 end
