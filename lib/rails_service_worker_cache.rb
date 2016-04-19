fe88b7fddb2af56cbf7311388794aa574820126d
Tony Edwards <tony@plymouthsoftware.com>
2016-04-19 09:52:17 +0100

    Move imports

--- lib/rails_service_worker_cache.rb
+++ lib/rails_service_worker_cache.rb
@@ -1,1 +1,3 @@
 require 'rails_service_worker_cache/rails'
+require 'rails_service_worker_cache/rails/engine' if defined?(Rails)
+require 'rails_service_worker_cache/rails/railtie' if defined?(Rails)
