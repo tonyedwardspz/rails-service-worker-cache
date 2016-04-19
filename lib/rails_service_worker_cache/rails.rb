4c699d0d849cb3ef61553dce94bda3b61f52aa7b
Tony Edwards <tony@plymouthsoftware.com>
2016-04-19 09:27:31 +0100

    Fix: requiring wrong module

--- lib/rails_service_worker_cache/rails.rb
+++ lib/rails_service_worker_cache/rails.rb
@@ -1,5 +1,5 @@
 require 'rails_service_worker_cache/rails/engine' if defined?(Rails)
-require 'rails_service_worker_cache/rails/version' if defined?(Rails)
+require 'rails_service_worker_cache/rails/railtie' if defined?(Rails)
 
 module RailsServiceWorkerCache
   module Rails
