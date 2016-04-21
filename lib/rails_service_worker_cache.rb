8807e9ce20ca84bb9282775ee6264e83ada930aa
Tony Edwards <tony@plymouthsoftware.com>
2016-04-21 14:43:51 +0100

    Adds configuration

--- lib/rails_service_worker_cache.rb
+++ lib/rails_service_worker_cache.rb
@@ -2,3 +2,22 @@ require 'rails_service_worker_cache/rails'
 require 'rails_service_worker_cache/rails/version'
 require 'rails_service_worker_cache/rails/engine' if defined?(Rails)
 require 'rails_service_worker_cache/rails/railtie' if defined?(Rails)
+
+module RailsServiceWorkerCache
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :assets
+
+    def initialize
+      @assets = []
+    end
+  end
+end
